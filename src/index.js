dcf704377d3a8bc339b6c1d73163a4f33dee23b3
grigorevadari <d.grig.d.20.04@gmail.com>
2021-03-15 23:40:51 +0300

    init:brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const stack = [];
+    const existBracket = {};
+    for(let j = 0; j < bracketsConfig.length; j += 1) {
+        existBracket[bracketsConfig[j][0]] = bracketsConfig[j][1];
+    }
+
+    stack.push(str.charAt(0));
+
+    for(let i = 1; i < str.length; i += 1) {
+        if(str.charAt(i) === existBracket[stack[stack.length - 1]]) {
+            stack.pop();
+        } else {
+            stack.push(str.charAt(i));
+        }
+    }
+    return stack.length <= 0;
+
+};
